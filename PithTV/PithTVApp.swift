cd74581a22e448973bae384fad595f3326b59378
ccantill <christoph.cantillon@unifly.aero>
2022-11-03 16:34:57 +0100

    Initial Commit

---
+++ PithTV/PithTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  PithTVApp.swift
+//  PithTV
+//
+//  Created by Christoph Cantillon on 03/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct PithTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
