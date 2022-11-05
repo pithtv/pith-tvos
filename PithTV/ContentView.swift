4882092e0ded445345811958265834bbeaf22e04
ccantill <christoph.cantillon@unifly.aero>
2022-11-05 12:52:32 +0100

    tweaks

--- PithTV/ContentView.swift
+++ PithTV/ContentView.swift
@@ -13,31 +13,42 @@ func playItem(item: ChannelItem) {
 
 struct ContentView: View {
     var body: some View {
-        HStack {
-            List(channels) { channel in
-                Text(channel.title)
-            }.frame(width: 250)
-            List(ribbons) {
-                ribbon in
-                Text(ribbon.name)
-                ScrollView(.horizontal) {
-                    HStack {
-                        ForEach(recentlyAdded, id: \.item.id) {
-                            ribbonItem in
-                            Button(action: {
-                                playItem(item: ribbonItem.item)
-                            }) {
-                                if let poster = ribbonItem.item.poster {
-                                    AsyncImage(url: URL(string: poster),
-                                               content: {img in img.image?
-                                            .resizable()
-                                            .aspectRatio(contentMode: .fit)
-                                        .frame(width: 200, height: 300)})
+            NavigationView {
+                ScrollView(.vertical) {
+                    VStack(alignment: .leading) {
+                        ScrollView(.horizontal) {
+                            HStack() {
+                                ForEach(channels) {
+                                    channel in
+                                    NavigationLink(channel.title, destination: Text("Test"))
+                                }
+                            }
+                        }
+                        ForEach(ribbons) {
+                            ribbon in
+                            Text(ribbon.name)
+                            ScrollView(.horizontal) {
+                                HStack {
+                                    ForEach(recentlyAdded, id: \.item.id) {
+                                        ribbonItem in
+                                        Button(action: {
+                                            playItem(item: ribbonItem.item)
+                                        }) {
+                                            if let poster = ribbonItem.item.poster {
+                                                AsyncImage(
+                                                    url: URL(string: poster),
+                                                    content: {img in img
+                                                        .resizable()
+                                                        .aspectRatio(contentMode: .fill)},
+                                                    placeholder: { ProgressView() })
+                                                .frame(width: 200, height: 300)
+                                            }
+                                        }.buttonStyle(CardButtonStyle())
+                                    }
                                 }
                             }
                         }
                     }
-                }
             }
         }
     }
